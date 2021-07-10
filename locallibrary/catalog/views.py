e317db853a21564b4635332a40cb864a1fb371cc
jarabt <jarabt@seznam.cz>
2021-07-10 09:12:58 +0200

    generic class view

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -25,3 +26,7 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, "catalog/index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
