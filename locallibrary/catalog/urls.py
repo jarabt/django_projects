53be0b027b39a1a2dee52581995f322293172ee0
Tesař Jaroslav <Tesar@cdis.cz>
2021-07-16 10:06:46 +0200

    part 8 finished incl. chalange yourself

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
         path('authors/', views.AuthorListView.as_view(), name='authors'),
         path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
         path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-        path('allborrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
+        path('borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
         ]
 
