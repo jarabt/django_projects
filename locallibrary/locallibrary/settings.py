7a2486281586abe1acfa846733ceee9163e5e758
Tesař Jaroslav <Tesar@cdis.cz>
2021-07-19 11:41:35 +0200

    first production commit: settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2v7yowr-lxp5j=homhrx77=tr!jwr8t@b10wb$o_fu6#hrkox'
+# SECRET_KEY = 'django-insecure-d2v7yowr-lxp5j=homhrx77=tr!jwr8t@b10wb$o_fu6#hrkox'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-d2v7yowr-lxp5j=homhrx77=tr!jwr8t@b10wb$o_fu6#hrkox')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False' 
 
 ALLOWED_HOSTS = []
 
